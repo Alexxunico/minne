05b69f4842725edcf16dc129cefc017efee542c8
Alexxunico <alexx.unico.2@gmail.com>
2025-02-11 11:18:49 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 figlet run miner
 echo "BTC donation address: 1GZHkonSjLeRfs8jL3QZP7Pg9zY2sc54MA "
 cd cpuminer-multi
-sudo ./cpuminer -o stratum+tcp://btc.poolbinance.com:1800 -u samsesh -p anything
+sudo ./cpuminer -o stratum+tcp://btc.poolbinance.com:1800 -u 123456 -p Alfaomega001.001
